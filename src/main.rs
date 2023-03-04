6d567a98caf44a81b8a021416055e66311c5fda0
matgd <28411337+matgd@users.noreply.github.com>
2023-03-04 23:45:20 +0100

    Initial stuff

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+
+    // git diff <commit> <commit> --name-only
+    // ex. git diff HEAD~1 --name-only
+    // ex. git diff HEAD~1 HEAD~2 --name-only
+    //
+    // git diff <commit> <commit> --stat
+    // ??
+    //
+    // git diff <commit> <commit> <file>
+    // ex. git diff HEAD~1 HEAD~2 main.rs
+}
